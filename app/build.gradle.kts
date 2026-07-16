e6f125d67b01ef4621cdb49a5a13180864694cee
MTS05 <sbhimavat21@gmail.com>
2026-07-16 18:51:47 +0530

    v5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.melodix.player"
         minSdk = 30
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
